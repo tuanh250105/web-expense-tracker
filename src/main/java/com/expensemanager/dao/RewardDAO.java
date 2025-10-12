d9a074633e4dc713a4fdb99ba175cf54f9611c68
MyTran <tranthitramyttii26092005@gmail.com>
2025-10-13 02:22:53 +0700

    sua

--- src/main/java/com/expensemanager/dao/RewardDAO.java
+++ src/main/java/com/expensemanager/dao/RewardDAO.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 
 public class RewardDAO {
 
-
     private static final EntityManagerFactory EMF =
             Persistence.createEntityManagerFactory("BudgetBuddyUnit");
 
